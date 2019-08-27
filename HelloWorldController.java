bbaba36c06ad5fb436179797a2f0713a27457d46
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-27 19:40:31 -0400

    Add files via upload

---
+++ HelloWorldController.java
@@ -0,0 +1,68 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldController
+ */
+@WebServlet("/HelloWorldController")
+public class HelloWorldController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private String greetMsg;                     
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		greetMsg = "Hello there!";
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/plain");
+		response.getWriter().append("My first servlet Hello World!");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+
+HELLO WORLD!!!
+</head>
+
+
+</html>
